d7c046816af2457c9854936d52c6ca9de8e6f52b
Yassine Larhrissi <72478988+YassineLr@users.noreply.github.com>
2022-10-24 12:26:11 +0100

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-
-int main()
-{
-    int x = ft_printf("%d\n %s\n", -2147483648, "Hello mtf!");
-    int y = printf("%d\n %s\n", -2147483648, "Hello mtf!");
-    printf("%d\n%d\n", x, y);
-}
