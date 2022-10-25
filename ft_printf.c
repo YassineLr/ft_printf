51611ecd0b8e98ed91c3bc3b659ec8c6e241d75f
ylarhris <y.larhrissi.2001@gmail.com>
2022-10-25 03:42:21 +0100

    Last dance

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,34 @@
 /*   By: ylarhris <ylarhris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 21:52:38 by ylarhris          #+#    #+#             */
-/*   Updated: 2022/10/25 01:51:28 by ylarhris         ###   ########.fr       */
+/*   Updated: 2022/10/25 03:37:54 by ylarhris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	specifier_handler(const char *str, va_list ap, int *count, int i)
+{
+	if (str[i] == 'd' || str[i] == 'i')
+		ft_putnbr(va_arg(ap, int), count);
+	else if (str[i] == 'u')
+		ft_putunbr(va_arg(ap, unsigned int), count);
+	else if (str[i] == 'c' )
+		ft_putchar(va_arg(ap, int), count);
+	else if (str[i] == '%')
+		ft_putchar('%', count);
+	else if (str[i] == 's')
+		ft_putstr(va_arg(ap, char *), count);
+	else if (str[i] == 'p')
+	{
+		ft_putstr("0x", count);
+		ft_printf_hexa_lowercase(va_arg(ap, long), count);
+	}
+	else if (str[i] == 'x')
+		ft_printf_hexa_lowercase(va_arg(ap, unsigned int), count);
+	else if (str[i] == 'X')
+		ft_printf_hexa_uppercase(va_arg(ap, int), count);
+}
 
 int	ft_printf(const char *str, ...)
 {
@@ -23,41 +46,10 @@ int	ft_printf(const char *str, ...)
 	count = 0;
 	while (str[i])
 	{
-		if (str[i] == '%' && (str[i + 1] == 'd' || str[i + 1] == 'i' ))
+		if (str[i] == '%')
 		{
-			ft_putnbr(va_arg(ap, int), &count);
 			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == 'c' )
-		{
-			ft_putchar(va_arg(ap, int), &count);
-			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == '%')
-		{
-			ft_putchar('%', &count);
-			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == 's')
-		{
-			ft_putstr(va_arg(ap, char *), &count);
-			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == 'p')
-		{
-			ft_putstr("0x", &count);
-			ft_printf_hexa_lowercase(va_arg(ap, long), &count);
-			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == 'd')
-		{
-			ft_putnbr(va_arg(ap, int), &count);
-			i++;
-		}
-		else if (str[i] == '%' && str[i + 1] == 'u')
-		{
-			ft_putunbr(va_arg(ap, unsigned int), &count);
-			i++;
+			specifier_handler(str, ap, &count, i);
 		}
 		else
 			ft_putchar(str[i], &count);
